a467c5b62468177f00ce172ab874f41d92a6ea8f
codealchemy <alchemitt@gmail.com>
2015-08-29 11:53:55 -0700

    Minor method name change to make it clear it's calling the index

--- pull_nation_data_to_csv.rb
+++ pull_nation_data_to_csv.rb
@@ -16,7 +16,7 @@ class NB::ApiClient
     @client.call(resource, :index, limit: 100)
   end
 
-  def get_page_resource_with_id(resource, id)
+  def get_page_resource_index_with_id(resource, id)
     @client.call(resource, :index, site_slug: @site_slug, id: id, limit: 100)
   end
 
@@ -39,13 +39,13 @@ class NB::CsvHandler
   end
 
   def retrieve_blog_post_headers(blog_id)
-    @client.get_page_resource_with_id(:blog_posts, blog_id)['results'].first.keys
+    @client.get_page_resource_index_with_id(:blog_posts, blog_id)['results'].first.keys
   end
 
   def create_blog_post_csv(blog_id)
     CSV.open("#{@client.slug}_blog_post_api_index.csv", 'w') do |csv|
       csv << retrieve_blog_post_headers(blog_id)
-      request = @client.get_page_resource_with_id(:blog_posts, blog_id)
+      request = @client.get_page_resource_index_with_id(:blog_posts, blog_id)
       page = @client.paginate(request)
       parse_data(page, csv)
     end
